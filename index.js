cb8d9fecbf6970117828cbd36667f2801b0d1544
Dee3w <joydanah>
2023-06-13 14:15:09 +0300

    arithmetic lab solution done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const randomNum = Math.floor(Math.random() * 100) + 1;
+console.log(randomNum); 
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+console.log(mod);
+
+const numberSet = [5, 20, 10, 15, 3];
+const maximum = numberSet[0];
+
+for(let i = 1; i < numberSet.length; i++) {
+    if (numberSet[i] > maximum);
+}
+console.log(maximum); 
